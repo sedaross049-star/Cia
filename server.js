e1d7a7bec36a74270df30edff8ee6a3ae2bdba45
sedaross049-star <sedaross049@gmail.com>
2026-09-10 21:06:21 +0300

    Change static file serving directory to root

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const io = new Server(server, {
 });
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // قواعد البيانات المؤقتة في الذاكرة (In-Memory Database)
 let users = [];
